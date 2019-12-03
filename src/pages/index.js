cde64ef62f7d289a9281b92d6988738b5ca5a7ae
Bayo Ade <bayo.300@gmail.com>
2019-12-03 01:55:50 +0000

    fixing some CSS shit

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,8 @@ const IndexPage = () => {
                     <h1 className={Abouty.aboutHead}>Coding Resources</h1>
                 </div>
                 <div className={Abouty.paragraph}>
-                   <p className={Abouty.para1}>The purpose of this website is to help people, primarily junior developers, by putting all the best web development resources in one place for easy access, especially in case we forget the name of fun tools we have used before.</p>
-
-<p>If you'd like to make any suggestions or if you have a helpful resource you would like featured here, contact me <a className={Abouty.noStyle} href="mailto:dev.bayo@gmail.com">dev.bayo@gmail.com</a></p>
+                   <p>The purpose of this website is to help people, primarily junior developers, by putting all the best web development resources in one place for easy access, especially in case we forget the name of fun tools we have used before.</p>
+                    <p>If you'd like to make any suggestions or if you have a helpful resource you would like featured here, contact me <a className={Abouty.noStyle} href="mailto:dev.bayo@gmail.com">dev.bayo@gmail.com</a></p>
                 </div>
                 </div>
                 </main>
