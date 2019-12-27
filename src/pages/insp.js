f5631dd2facc90a4785965f2979934a37158f76e
Bayo Ade <bayo.300@gmail.com>
2019-12-27 23:56:06 +0000

    changing all fonts style in paragraph for consistency

--- src/pages/insp.js
+++ src/pages/insp.js
@@ -1,13 +1,2 @@
 import React from 'react'
 import Layout from '../components/layout'
-
-const Insp = () => {
-    return ( <
-        Layout >
-        <
-        h1 > This is the Inspiration page < /h1> <
-        /Layout>
-    )
-}
-
-export default Insp
