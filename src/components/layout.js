4d210393011db447e3ecfdba0fda8eb02feb8f88
Bayo Ade <bayo.300@gmail.com>
2019-12-29 17:15:10 +0000

    fixing stuff. trying to make the deploy work

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,41 +10,42 @@ class Layout extends React.Component {
     state = {
         SideDrawerOpen: false
     };
-    
+
     drawerToggleClickHandler = () => {
-      this.setState((prevState) => {
-          return {SideDrawerOpen: !prevState.SideDrawerOpen};
-      });  
+        this.setState((prevState) => {
+            return { SideDrawerOpen: !prevState.SideDrawerOpen };
+        });
     };
 
     backdropClickHandler = () => {
-        this.setState({SideDrawerOpen: false});
+        this.setState({ SideDrawerOpen: false });
     }
 
-    
+
     render() {
 
-    let sideDrawer;
-    let backDrop;
+        let sideDrawer;
+        let backDrop;
 
-    if(this.state.SideDrawerOpen) {
-        sideDrawer = <SideDrawer/>;
-        backDrop = <Backdrop click={this.backdropClickHandler}/>
-    }
-    const {children} = this.props;
-    return (
-        <div className={layoutStyles.container}>
-            <Header drawerClickHandler={this.drawerToggleClickHandler}/>
-            {sideDrawer}
-            {backDrop}
-            {children}
-            <Footer />
-        </div>
-    )
-        
+        if (this.state.SideDrawerOpen) {
+            sideDrawer = < SideDrawer / > ;
+            backDrop = < Backdrop click = { this.backdropClickHandler }
+            />
+        }
+        const { children } = this.props;
+        return ( <
+            div className = { layoutStyles.container } >
+            <
+            Header drawerClickHandler = { this.drawerToggleClickHandler }
+            /> { sideDrawer } { backDrop } { children } <
+            Footer / >
+            <
+            /div>
+        )
+
     }
 }
-    
+
 
 
 export default Layout
